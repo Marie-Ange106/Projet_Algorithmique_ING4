8303bc56f807196aef44eb7c1972f32cd5e2bab2
richelle777 <richellemayap@gmail.com>
2022-12-26 09:24:12 +0100

    version avec ajout de firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule,HttpClient} from '@angular/common/http';
 
+// Firebase + environment
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule,AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireStorageModule,],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },HttpClient],
   bootstrap: [AppComponent],
 })
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,20 @@
 import { Forfait } from './../class/forfait';
 import { Component } from '@angular/core';
 import { ForfaitsService } from '../services/forfaits.service';
+import { finalize, tap } from 'rxjs/operators';
+import { AngularFireStorage, AngularFireUploadTask,
+} from '@angular/fire/compat/storage';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+
+export interface imgFile {
+  name: string;
+  filepath: string;
+  size: number;
+}
 
 @Component({
   selector: 'app-tab2',
@@ -8,28 +22,111 @@ import { ForfaitsService } from '../services/forfaits.service';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+
+  // File upload task
+  fileUploadTask!: AngularFireUploadTask;
+  // Upload progress
+  percentageVal!: Observable<number | undefined>;
+  // Track file uploading with snapshot
+  trackSnapshot!: Observable<any>;
+  // Uploaded File URL
+  UploadedImageURL!: Observable<string>;
+  // Uploaded image collection
+  files!: Observable<imgFile[]>;
+  // Image specifications
+  imgName!: string;
+  imgSize!: number;
+  // File uploading status
+  isFileUploading!: boolean;
+  isFileUploaded!: boolean;
+  private filesCollection: AngularFirestoreCollection<imgFile>;
+
    search = false;
    forfaits:Forfait[] = [];
    forfaitsPerOp:Forfait[] = [];
    operateurs = new Set();
    selectOp  = 'Mango';
-   constructor(private forfaitsService:ForfaitsService) { }
-   
-   ionViewDidEnter(){
-     //subcrive recure la valeur et la met dans reponse
-     this.forfaitsService.getForfait().subscribe(
-       (reponse: Forfait[])=>{
-         this.forfaits=reponse
-         this.forfaits.forEach(
-          data => {
-            this.operateurs.add(data.operateur)
+  constructor( private afs: AngularFirestore,private afStorage: AngularFireStorage, private forfaitsService:ForfaitsService) {
+    this.isFileUploading = false;
+    this.isFileUploaded = false;
+    // Define uploaded files collection
+    this.filesCollection = afs.collection<imgFile>('imagesCollection');
+    this.files = this.filesCollection.valueChanges();
+  }
+
+  uploadImage(event: FileList) {
+    const file = event.item(0);
+    // Image validation
+    if (file!.type.split('/')[0] !== 'image') {
+      console.log('File type is not supported!');
+      return;
+    }
+    this.isFileUploading = true;
+    this.isFileUploaded = false;
+    this.imgName = file!.name;
+    // Storage path
+    const fileStoragePath = `filesStorage/${new Date().getTime()}_${file!.name}`;
+    // Image reference
+    const imageRef = this.afStorage.ref(fileStoragePath);
+    // File upload task
+    this.fileUploadTask = this.afStorage.upload(fileStoragePath, file);
+    // Show uploading progress
+    this.percentageVal = this.fileUploadTask.percentageChanges();
+    this.trackSnapshot = this.fileUploadTask.snapshotChanges().pipe(
+      finalize(() => {
+        // Retreive uploaded image storage path
+        this.UploadedImageURL = imageRef.getDownloadURL();
+        this.UploadedImageURL.subscribe(
+          (resp) => {
+            this.storeFilesFirebase({
+              name: file!.name,
+              filepath: resp,
+              size: this.imgSize,
+            });
+            this.isFileUploading = false;
+            this.isFileUploaded = true;
+          },
+          (error) => {
+            console.log(error);
           }
-         )
-         this.forfaitsPerOp = this.forfaits.filter(forfait => forfait.operateur == this.selectOp);
-       }
-     )
-     
-   };
+        );
+      }),
+      tap((snap:any) => {
+        this.imgSize = snap.totalBytes;
+      })
+    );
+  }
+   
+  storeFilesFirebase(image: imgFile) {
+    const fileId = this.afs.createId();
+    this.filesCollection
+      .doc(fileId)
+      .set(image)
+      .then((res:any) => {
+        console.log(res);
+      })
+      .catch((err:any) => {
+        console.log(err);
+      });
+  }
+
+  ionViewDidEnter(){
+    //subcrive recure la valeur et la met dans reponse
+    this.forfaitsService.getForfait().subscribe(
+      (reponse: Forfait[])=>{
+        this.forfaits=reponse
+        this.forfaits.forEach(
+        data => {
+          this.operateurs.add(data.operateur)
+        }
+        )
+        console.log(this.forfaits);
+        
+        this.forfaitsPerOp = this.forfaits.filter(forfait => forfait.operateur == this.selectOp);
+      }
+    )
+    
+  };
 
 
   search_package(){
@@ -41,4 +138,7 @@ export class Tab2Page {
     this.forfaitsPerOp = this.forfaits.filter(forfait => forfait.operateur == operator);
   }
 
+  openModal(){
+
+  }
 }
